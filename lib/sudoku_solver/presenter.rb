85c365e1d8f9b63daab30b4f840ef14e1e474a41
ha <ha.nguyenk07@gmail.com>
2016-06-22 22:44:21 +0700

    Add presenter.

--- lib/sudoku_solver/presenter.rb
+++ lib/sudoku_solver/presenter.rb
@@ -1,4 +1,20 @@
 module SudokuSolver
   class Presenter
+    def initialize(matrix)
+      @matrix = matrix
+    end
+
+    def show
+      border = "+-----+-----+-----+"
+      9.times do |i|
+        puts border if i%3 == 0
+        9.times do |j|
+          print j%3 == 0 ? "|" : " "
+          print @matrix[i][j] == 0 ? "." : @matrix[i][j]
+        end
+        puts "|"
+      end
+      puts border
+    end
   end
 end
