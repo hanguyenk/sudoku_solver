77258f0402c24b2a99cabf1c6c8f3b4706620bab
ha <ha.nguyenk07@gmail.com>
2016-06-22 23:39:21 +0700

    Add entrypoint feature.

--- lib/sudoku_solver.rb
+++ lib/sudoku_solver.rb
@@ -1,4 +1,18 @@
+require_relative 'sudoku_solver/configuration'
+require_relative 'sudoku_solver/engine'
+require_relative 'sudoku_solver/parser'
+require_relative 'sudoku_solver/presenter'
+
 module SudokuSolver
   def self.run(str)
+    configuration = SudokuSolver::Configuration.new
+    puzzle = SudokuSolver::Parser.new(configuration).parse(str)
+    result = SudokuSolver::Engine.new(puzzle).run
+
+    if result.nil?
+      puts 'The inputted puzzle could not be resolved.'
+    else
+      SudokuSolver::Presenter.new(result).show
+    end
   end
 end
