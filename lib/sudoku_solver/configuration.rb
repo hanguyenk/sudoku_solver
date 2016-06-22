7bde4e2c0e0924faae136e107d6831766500dd96
ha <ha.nguyenk07@gmail.com>
2016-06-22 21:08:56 +0700

    Add configuration feature.

--- lib/sudoku_solver/configuration.rb
+++ lib/sudoku_solver/configuration.rb
@@ -1,4 +1,10 @@
 module SudokuSolver
   class Configuration
+    attr_accessor :row_delimiter, :hidden_place_symbol
+
+    def initialize(row_delimiter = '|', hidden_place_symbol = '.')
+      @row_delimiter = row_delimiter
+      @hidden_place_symbol = hidden_place_symbol
+    end
   end
 end
